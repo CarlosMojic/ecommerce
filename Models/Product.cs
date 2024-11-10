b4f3b53b6a30494bc3f5411604fa92bd2d3eda76
CarlosMojic <stevenmojica1gc@gmail.com>
2024-11-10 10:13:29 -0600

    Cambios al carrito

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,5 +11,8 @@ namespace ecomerce.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+
+        public int Quantity { get; set; } = 1;
+        public string Brand { get; set; }
     }
 }
