6d7753ba51e39f43dd99fde75dc962a31255c299
CarlosMojic <stevenmojica1gc@gmail.com>
2024-11-10 10:37:32 -0600

    Add images from view carrito

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -43,36 +43,36 @@ namespace ecomerce.Controllers
             // Aquí puedes simular una lista de productos o recuperarlos de una base de datos
             var products = new List<Product>
             {
-                new Product { ProductId = 1, Name = "Consola de PS4 Pro 1TB Negro", Price = 2000m},
-                new Product { ProductId = 2, Name = "HP Laptop 15-EF1019LA", Price = 200m},
-                new Product { ProductId = 3, Name = "Televisor 65 4K Ultra HD Smart TV", Price = 500m},
-                new Product { ProductId = 4, Name = "Televisor 50 4K Ultra HD Smart Android", Price = 350m},
-                new Product { ProductId = 5, Name = "Smartphone Xiaomi Redmi Note 10", Price = 200m},
-                new Product { ProductId = 6, Name = "Smartphone Samsung Galaxy A52", Price = 300m},
-                new Product { ProductId = 7, Name = "Auriculares Sony WH-1000XM4", Price = 20m},
-                new Product { ProductId = 8, Name = "Microsoft Surface Pro 7", Price = 900m},
+                new Product { ProductId = 1, Name = "Consola de PS4 Pro 1TB Negro", Price = 2000m, ImageUrl ="/images/Tec/Consola de PS4 Pro 1TB Negro.jpg"},
+                new Product { ProductId = 2, Name = "HP Laptop 15-EF1019LA", Price = 200m, ImageUrl ="/images/Tec/HP Laptop 15-EF1019LA.jpg"},
+                new Product { ProductId = 3, Name = "Televisor 65 4K Ultra HD Smart TV", Price = 500m, ImageUrl ="/images/Tec/Televisor 65 4K Ultra HD Smart TV.jpeg"},
+                new Product { ProductId = 4, Name = "Televisor 50 4K Ultra HD Smart Android", Price = 350m, ImageUrl ="/images/Tec/Televisor 50 4K Ultra HD Smart Android.jpg"},
+                new Product { ProductId = 5, Name = "Smartphone Xiaomi Redmi Note 10", Price = 200m, ImageUrl ="/images/Tec/Smartphone Xiaomi Redmi Note 10.jpg"},
+                new Product { ProductId = 6, Name = "Smartphone Samsung Galaxy A52", Price = 300m, ImageUrl ="/images/Tec/Smartphone Samsung Galaxy A52.jpeg"},
+                new Product { ProductId = 7, Name = "Auriculares Sony WH-1000XM4", Price = 20m, ImageUrl ="/images/Tec/Auriculares Sony WH-1000XM4.jpg"},
+                new Product { ProductId = 8, Name = "Microsoft Surface Pro 7", Price = 900m, ImageUrl ="/images/Tec/Microsoft Surface Pro 7.jpg"},
                 
                 //Seccion de ropa
 
-                new Product { ProductId = 9, Name = "abrigo", Price = 20m},
-                new Product { ProductId = 10, Name = "Blusa", Price = 10m},
-                new Product { ProductId = 11, Name = "Camiseta", Price = 5m},
-                new Product { ProductId = 12, Name = "Jean", Price = 8m},
-                new Product { ProductId = 13, Name = "Pantalon de algodon", Price = 8m},
-                new Product { ProductId = 14, Name = "Roopa deportiva", Price = 10m},
-                new Product { ProductId = 15, Name = "Sudadera", Price = 20m},
-                new Product { ProductId = 16, Name = "Vestido casual", Price = 10m},
+                new Product { ProductId = 9, Name = "abrigo", Price = 20m, ImageUrl ="/images/Ropa/Abrigo.jpg"},
+                new Product { ProductId = 10, Name = "Blusa", Price = 10m, ImageUrl ="/images/Ropa/Blusa.jpg"},
+                new Product { ProductId = 11, Name = "Camiseta", Price = 5m, ImageUrl ="/images/Ropa/Camiseta.jpg"},
+                new Product { ProductId = 12, Name = "Jean", Price = 8m, ImageUrl ="/images/Ropa/Jean.jpeg"},
+                new Product { ProductId = 13, Name = "Pantalon de algodon", Price = 8m, ImageUrl ="/images/Ropa/Pantalon de algodon.jpg"},
+                new Product { ProductId = 14, Name = "Ropa deportiva", Price = 10m, ImageUrl ="/images/Ropa/Ropa deportiva.jpg"},
+                new Product { ProductId = 15, Name = "Sudadera", Price = 20m, ImageUrl ="/images/Ropa/Sudadera.jpg"},
+                new Product { ProductId = 16, Name = "Vestido casual", Price = 10m, ImageUrl ="/images/Ropa/Vestido casual.jpg"},
 
                 //Seccion de Herramientas
 
-                new Product { ProductId = 17, Name = "Alicates", Price = 1m},
-                new Product { ProductId = 18, Name = "Cinta", Price = 2m},
-                new Product { ProductId = 19, Name = "Destonillador", Price = 5m},
-                new Product { ProductId = 20, Name = "Llave inglesa", Price = 10m},
-                new Product { ProductId = 21, Name = "Martillo", Price = 10m},
-                new Product { ProductId = 22, Name = "Sierra", Price = 5},
-                new Product { ProductId = 23, Name = "Taladro", Price = 30},
-                new Product { ProductId = 24, Name = "Cuter", Price = 1},
+                new Product { ProductId = 17, Name = "Alicates", Price = 1m, ImageUrl ="/images/her/alicates.jpeg"},
+                new Product { ProductId = 18, Name = "Cinta", Price = 2m, ImageUrl ="/images/her/cinta.jpg"},
+                new Product { ProductId = 19, Name = "Destonillador", Price = 5m, ImageUrl ="/images/her/destornillador.jpg"},
+                new Product { ProductId = 20, Name = "Llave inglesa", Price = 10m, ImageUrl ="/images/her/inglesa.jpg"},
+                new Product { ProductId = 21, Name = "Martillo", Price = 10m, ImageUrl ="/images/her/martillo.jpg"},
+                new Product { ProductId = 22, Name = "Sierra", Price = 5, ImageUrl ="/images/her/sierra.jpg"},
+                new Product { ProductId = 23, Name = "Taladro", Price = 30, ImageUrl ="/images/her/taladro.jpg"},
+                new Product { ProductId = 24, Name = "Cuter", Price = 1, ImageUrl ="/images/her/cuter.jpg"}
                 
 
                 
