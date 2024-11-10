a4a3e236b3ea8dbefb23a6af5daa280917186289
CarlosMojic <stevenmojica1gc@gmail.com>
2024-11-10 00:27:39 -0600

    Ajuste a las referencias de herramientas

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -65,14 +65,14 @@ namespace ecomerce.Controllers
 
                 //Seccion de Herramientas
 
-                new Product { ProductId = 17, Name = "abrigo", Price = 20m},
-                new Product { ProductId = 18, Name = "abrigo", Price = 10m},
-                new Product { ProductId = 19, Name = "abrigo", Price = 10m},
-                new Product { ProductId = 20, Name = "abrigo", Price = 10m},
-                new Product { ProductId = 21, Name = "abrigo", Price = 10m},
-                new Product { ProductId = 22, Name = "abrigo", Price = 10m},
-                new Product { ProductId = 23, Name = "abrigo", Price = 10m},
-                new Product { ProductId = 24, Name = "abrigo", Price = 10m},
+                new Product { ProductId = 17, Name = "alicates", Price = 1m},
+                new Product { ProductId = 18, Name = "cinta", Price = 2m},
+                new Product { ProductId = 19, Name = "destonillador", Price = 5m},
+                new Product { ProductId = 20, Name = "llave inglesa", Price = 10m},
+                new Product { ProductId = 21, Name = "martillo", Price = 10m},
+                new Product { ProductId = 22, Name = "sierra", Price = 5},
+                new Product { ProductId = 23, Name = "taladro", Price = 30},
+                new Product { ProductId = 24, Name = "cuter", Price = 1},
                 
 
                 
